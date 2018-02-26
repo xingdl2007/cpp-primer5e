8cb9a2e44eb983ad5d74b89ef5e73cbd9a083c10
XingDL <xingdl2007@gmail.com>
2018-02-26 20:24:12 +0800

    chapter6: Designing lock-based concurrency data structures

---
+++ src/concurrency/chapter6/list/list.cc
@@ -0,0 +1,100 @@
+//
+// C++ concurrency in Action.
+//
+
+#include <memory>
+#include <mutex>
+
+#include <iostream>
+
+template <typename T>
+class threadsafe_list {
+  struct node {
+    std::mutex m;
+    std::shared_ptr<T> data;
+    std::unique_ptr<node> next;
+
+    node() = default;
+    node(T const &value) : data(std::make_shared<T>(value)) {}
+  };
+  node head;
+
+public:
+  threadsafe_list() = default;
+  ~threadsafe_list() {
+    remove_if([](T const &) { return true; });
+  }
+
+  threadsafe_list(threadsafe_list const &other) = delete;
+  threadsafe_list &operator=(threadsafe_list const &other)= delete;
+
+  void push_front(T const &value) {
+    std::unique_ptr<node> new_node(new node(value));
+    std::lock_guard<std::mutex> lk(head.m);
+    new_node->next = std::move(head.next);
+    head.next = std::move(new_node);
+  }
+
+  template <typename Function>
+  void for_each(Function f) {
+    node *current = &head;
+    std::unique_lock<std::mutex> lk(head.m);
+    while (node *const next = current->next.get()) {
+      std::unique_lock<std::mutex> next_lk(next->m);
+      lk.unlock();
+      f(*next->data);
+      current = next;
+      lk = std::move(next_lk);
+    }
+  }
+
+  template <typename Predicate>
+  std::shared_ptr<T> find_first_if(Predicate p) {
+    node *current = &head;
+    std::unique_lock<std::mutex> lk(head.m);
+    while (node *const next = current->next.get()) {
+      std::unique_lock<std::mutex> next_lk(next->m);
+      lk.unlock();
+      if (p(*next->data)) {
+        return next->data;
+      }
+      current = next;
+      lk = std::move(next_lk);
+    }
+    return std::shared_ptr<T>();
+  }
+
+  template <typename Predicate>
+  void remove_if(Predicate p) {
+    node *current = &head;
+    std::unique_lock<std::mutex> lk(head.m);
+    while (node *const next = current->next.get()) {
+      std::unique_lock<std::mutex> next_lk(next->m);
+      if (p(*next->data)) {
+        // should unlock next_lk before destroy "std::unique_ptr<node> old_next"
+        std::unique_ptr<node> old_next = std::move(current->next);
+        current->next = std::move(next->next);
+        next_lk.unlock();
+      } else {
+        lk.unlock();
+        current = next;
+        lk = std::move(next_lk);
+      }
+    }
+  }
+};
+
+int main() {
+  threadsafe_list<int> ls;
+  ls.push_front(1);
+  ls.push_front(2);
+  ls.push_front(3);
+  ls.push_front(4);
+  ls.push_front(5);
+
+  ls.for_each([](int i) { std::cout << i << std::endl; });
+  ls.remove_if([](int i) { return i == 3; });
+  std::cout << std::endl;
+  ls.for_each([](int i) { std::cout << i << std::endl; });
+  return 0;
+}
--- src/concurrency/chapter6/queue5/queue.cc
+++ src/concurrency/chapter6/queue5/queue.cc
@@ -22,20 +22,54 @@ private:
 
   std::mutex tail_mutex;
   node *tail = nullptr;
+  std::condition_variable data_cond;
 
   node *get_tail() {
     std::lock_guard<std::mutex> lock(tail_mutex);
     return tail;
   }
 
+  // should be called when holding head lock
   std::unique_ptr<node> pop_head() {
-    std::lock_guard<std::mutex> lock(head_mutex);
-    if (head.get() == get_tail()) {
-      return nullptr;
-    }
     std::unique_ptr<node> old_head = std::move(head);
     head = std::move(old_head->next);
     return old_head;
+  }
+
+  // make sure queue is not empty, or it will block
+  std::unique_lock<std::mutex> wait_for_data() {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    data_cond.wait(lock, [this]() { return head.get() != get_tail(); });
+    return std::move(lock);
+  }
+
+  // move ownership of lock
+  std::unique_ptr<node> wait_pop_head() {
+    std::unique_lock<std::mutex> lock(wait_for_data());
+    return pop_head();
+  }
+
+  std::unique_ptr<node> wait_pop_head(T &value) {
+    std::unique_lock<std::mutex> lock(wait_for_data());
+    value = std::move(*head->data);
+    return pop_head();
+  }
+
+  std::unique_ptr<node> try_pop_head() {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    if (head.get() == get_tail()) {
+      return nullptr;
+    }
+    return pop_head();
+  }
+
+  std::unique_ptr<node> try_pop_head(T &value) {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    if (head.get() == get_tail()) {
+      return nullptr;
+    }
+    value = std::move(*head->data);
+    return pop_head();
   }
 
 public:
@@ -44,19 +78,55 @@ public:
   queue &operator=(const queue &) = delete;
 
   std::shared_ptr<T> try_pop() {
-    auto old_head = pop_head();
+    auto old_head = try_pop_head();
     return old_head ? old_head->data : nullptr;
   }
+
+  bool try_pop(T &value) {
+    auto old_head = try_pop_head(value);
+    return old_head != nullptr;
+  }
+
+  bool empty() {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    return head.get() == get_tail();
+  }
+
+  std::shared_ptr<T> wait_and_pop() {
+    auto old_head = wait_pop_head();
+    return old_head->data;
+  }
+
+  void wait_and_pop(T &value) {
+    auto old_head = wait_pop_head(value);
+  }
+
+  // all-in-one solution
+//  std::shared_ptr<T> wait_and_pop() {
+//    std::unique_lock<std::mutex> lock(head_mutex);
+//    std::unique_ptr<node> old_head;
+//    data_cond.wait(lock, [&, this]() {
+//      if (head.get() == get_tail()) {
+//        return false;
+//      }
+//      old_head = std::move(head);
+//      head = std::move(old_head->next);
+//      return true;
+//    });
+//    return old_head->data;
+//  }
 
   void push(T data) {
     std::shared_ptr<T> new_data = std::make_shared<T>(std::move(data));
     std::unique_ptr<node> p(new node);
     node *const new_tail = p.get();
-
-    std::lock_guard<std::mutex> lock(tail_mutex);
-    tail->data = new_data;
-    tail->next = std::move(p);
-    tail = new_tail;
+    {
+      std::lock_guard<std::mutex> lock(tail_mutex);
+      tail->data = new_data;
+      tail->next = std::move(p);
+      tail = new_tail;
+    }
+    data_cond.notify_one();
   }
 };
 
