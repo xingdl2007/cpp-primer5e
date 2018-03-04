f544a12959d9c11f86286d4fc519682e26851342
XingDL <xingdl2007@gmail.com>
2018-03-04 19:15:14 +0800

    chapter6: Designing lock-based concurrent data structures

--- src/concurrency/chapter6/stack/stack.cc
+++ src/concurrency/chapter6/stack/stack.cc
@@ -8,27 +8,21 @@
 #include <stack>
 #include <iostream>
 
-struct empty_stack : std::exception {
-  const char *what() const throw() override {
-    return "empty stack.";
-  }
-};
-
 template <typename T>
-class threadsafe_stack {
+class thread_safe_stack {
 private:
   std::stack<T> data;
   mutable std::mutex m;
 
 public:
-  threadsafe_stack() = default;
+  thread_safe_stack() = default;
 
   // copy-ctor, important to held source lock across copy
-  threadsafe_stack(const threadsafe_stack &other) {
+  thread_safe_stack(const thread_safe_stack &other) {
     std::lock_guard<std::mutex> lock(other.m);
     data = other.data;
   }
-  threadsafe_stack &operator=(const threadsafe_stack &) = delete;
+  thread_safe_stack &operator=(const thread_safe_stack &) = delete;
 
   void push(T new_value) {
     std::lock_guard<std::mutex> lock(m);
@@ -38,7 +32,7 @@ public:
   std::shared_ptr<T> pop() {
     std::lock_guard<std::mutex> lock(m);
     if (data.empty()) {
-      throw empty_stack();
+      return nullptr;
     }
     // if T support move-ctor
     std::shared_ptr<T> res = std::make_shared<T>(std::move(data.top()));
@@ -46,14 +40,15 @@ public:
     return res;
   }
 
-  void pop(T &value) {
+  bool pop(T &value) {
     std::lock_guard<std::mutex> lock(m);
     if (data.empty()) {
-      throw empty_stack();
+      return false;
     }
     // if T support move-assignment
     value = std::move(data.top);
     data.pop();
+    return true;
   }
 
   bool empty() const {
@@ -64,21 +59,15 @@ public:
 
 // cout is not thread safe, so output will interleaved
 template <typename T>
-void worker(threadsafe_stack<T> &stack) {
+void worker(thread_safe_stack<T> &stack) {
   int count = 0;
-  try {
-    while (true) {
-      stack.pop();
-      count++;
-    }
-  } catch (empty_stack) {
-    std::cout << "thread #" << std::this_thread::get_id() << " pop "
-              << std::dec << count << " values" << std::endl;
+  while (stack.pop()) {
+    count++;
   }
 }
 
 int main() {
-  threadsafe_stack<int> stack;
+  thread_safe_stack<int> stack;
   for (int i = 0; i < 1000; ++i) {
     stack.push(i);
   }
