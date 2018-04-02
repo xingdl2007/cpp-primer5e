b9b7aae730830d7a94d7d75ce9cabeaf472750c0
xingdl2007 <xingdl2007@gmail.com>
2018-04-02 15:35:22 +0800

    concurrency: overloaded

--- src/concurrency/chapter4/forward/forward.cc
+++ src/concurrency/chapter4/forward/forward.cc
@@ -2,6 +2,25 @@
 // C++ concurrency in Action.
 //
 
+// what is the difference between std::move and std::forward?
+// highlight:
+// std::forward is for perfect forwarding, std::move just allow you treat
+// a object as a temporary(an rvalue).
+//
+// ref: https://stackoverflow.com/questions/9671749/whats-the-difference-between-stdmove-and-stdforward
+//
+// std::move takes an object and allows you to treat it as a temporary (an rvalue).
+// Although it isn't a semantic requirement, typically a function accepting a reference
+// to an rvalue will invalidate it.
+// When you see std::move, it indicates that the value of the object should not be used
+// afterwards, but you can still assign a new value and continue using it.
+//
+// std::forward has a single use case: to cast a templated function parameter (inside the
+// function) to the value category (lvalue or rvalue) the caller used to pass it. This
+// allows rvalue arguments to be passed on as rvalues, and lvalues to be passed on as
+// lvalues, a scheme called "perfect forwarding."
+//
+
 #include <future>
 #include <iostream>
 
