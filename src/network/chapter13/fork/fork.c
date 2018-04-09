b1832c27d7431a2f8e72a12cb3d8370c7a918c92
程嵇邢滑 <xingdl2007@gmail.com>
2018-04-09 20:06:00 +0800

    network: chapter13: SIGCHLD demo

--- src/network/chapter13/fork/fork.c
+++ src/network/chapter13/fork/fork.c
@@ -3,11 +3,23 @@
 //
 
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
+
+void sig_handler(int sig) {
+  pid_t pid;
+  int stat;
+  if ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+    printf("child process exit: %d\n", stat);
+  }
+}
 
 int main() {
+  signal(SIGCHLD, sig_handler);
   pid_t pid = fork();
+
   if (pid == 0) {
     printf("child: %d\n", getpid());
   } else {
