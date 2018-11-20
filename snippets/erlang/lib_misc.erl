2f5e7f83996a458c2546bff97f2d5fd25cc50c43
程嵇邢滑 <xingdl2007@gmail.com>
2018-11-20 22:54:27 +0800

    erlang: update

--- snippets/erlang/lib_misc.erl
+++ snippets/erlang/lib_misc.erl
@@ -1,5 +1,27 @@
 -module(lib_misc).
--export([for/3]).
 
-for(Max,Max,F) -> [F(Max)];
-for(I,Max,F) -> [F(I)|for(I+1, Max, F)].
+-export([for/3, pythag/1, qsort/1, perm/1, max/2]).
+
+% for loop
+for(Max, Max, F) -> [F(Max)];
+for(I, Max, F) -> [F(I) | for(I + 1, Max, F)].
+
+% quick sort implementation
+qsort([]) -> [];
+qsort([Pivot | T]) ->
+    qsort([X || X <- T, X < Pivot]) ++
+      [Pivot] ++ qsort([X || X <- T, X >= Pivot]).
+
+% misc
+pythag(N) ->
+    [{A, B, C}
+     || A <- lists:seq(1, N), B <- lists:seq(1, N),
+	C <- lists:seq(1, N), A + B + C =< N,
+	A * A + B * B =:= C * C].
+
+% simple permutation implementation
+perm([]) -> [[]];
+perm(L) -> [[H | T] || H <- L, T <- perm(L -- [H])].
+
+max(X,Y) when X > Y -> X;
+max(_,Y) -> Y.
